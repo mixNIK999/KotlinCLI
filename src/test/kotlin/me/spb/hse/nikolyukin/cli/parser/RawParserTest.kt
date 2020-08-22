83446a6f079c74ead79f323325c67dcb8a9f5741
Mikhail Nikolyukin <michael.nik999@gmail.com>
2020-08-22 19:40:21 +0300

    fix kotlin stdlib version

--- src/test/kotlin/me/spb/hse/nikolyukin/cli/parser/RawParserTest.kt
+++ src/test/kotlin/me/spb/hse/nikolyukin/cli/parser/RawParserTest.kt
@@ -1,12 +1,10 @@
 package me.spb.hse.nikolyukin.cli.parser
 
-import com.github.h0tk3y.betterParse.grammar.parseToEnd
+import me.spb.hse.nikolyukin.cli.parser.commands.words.RawParser
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
-import org.junit.jupiter.params.provider.ValueSource
 import java.util.stream.Stream
 
 internal class RawParserTest {
@@ -14,7 +12,7 @@ internal class RawParserTest {
     @MethodSource("provideTestArguments")
     fun `word splitting`(rawString: String, expectedTokens: List<String>) {
         val parser = RawParser()
-        assertEquals(expectedTokens, parser.parseToEnd(rawString).map { it.text })
+        assertEquals(expectedTokens, parser.parse(rawString).map { it.text }.toList())
     }
 
     companion object {
