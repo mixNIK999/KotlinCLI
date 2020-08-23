8a3f6b3ac4e45581b945c903464eba9cd2554728
Mikhail Nikolyukin <michael.nik999@gmail.com>
2020-08-23 22:04:49 +0300

    refactor MainWordParser to WordParserWithExtension

--- src/main/kotlin/me/spb/hse/nikolyukin/cli/parser/words/WordParserWithExtension.kt
+++ src/main/kotlin/me/spb/hse/nikolyukin/cli/parser/words/WordParserWithExtension.kt
@@ -1,8 +1,8 @@
 package me.spb.hse.nikolyukin.cli.parser.words
 
-class MainWordParser(
-    val initParser : WordParser,
-    val extensions: Extension
+class WordParserWithExtension(
+    private val initParser: WordParser,
+    private val extensions: Extension
 ) : WordParser {
 
     override fun parse(rawString: String): Sequence<Word> = extensions.extend(initParser.parse(rawString))
